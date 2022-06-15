42d6b72d1d7891d553f1a802dcfc5947f1f98028
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-06-15 01:00:52 -0700

    branches operator form input and selects function

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,19 @@ function handleCalculation(event) {
   const number1 = parseInt(document.querySelector("input#input1").value);
   const number2 = parseInt(document.querySelector("input#input2").value);
   const operator = document.querySelector("input[name='operator']:checked").value;
+
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+
+  document.getElementById("output").innerText = result;
 }
 
 window.addEventListener("load", function() {
