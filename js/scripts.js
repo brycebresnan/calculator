bd9b15938609cf5d67438bb99f02e6b0b87d137d
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-06-15 00:43:57 -0700

    add form input retrival ability and operator selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
-// business logic
+// Business Logic
 function add(number1, number2) {
   return number1 + number2;
 }
 
-// user interface logic 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-window.alert(add(number1,number2));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// User interface logic 
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
 
